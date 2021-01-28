8ec6945efe2dffff0f339b2ba17196b38e3a5d4e
HeoSeungYeon <dia0312@naver.com>
2021-01-28 14:28:34 +0900

    JdbcTemplateRepository Study

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,6 +1,7 @@
 package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,22 +9,28 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 @Transactional
 class MemberServiceIntegrationTest {
+
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
+
     @Test
     public void 회원가입() throws Exception {
         //Given
         Member member = new Member();
         member.setName("hello");
+
         //When
         Long saveId = memberService.join(member);
+
         //Then
         Member findMember = memberRepository.findById(saveId).get();
         assertEquals(member.getName(), findMember.getName());
     }
+
     @Test
     public void 중복_회원_예외() throws Exception {
         //Given
@@ -31,6 +38,7 @@ class MemberServiceIntegrationTest {
         member1.setName("spring");
         Member member2 = new Member();
         member2.setName("spring");
+
         //When
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
